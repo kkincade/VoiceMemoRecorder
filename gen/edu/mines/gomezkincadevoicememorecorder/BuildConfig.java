de0d1915580d7c71c45025f56c61ba20831aeaf4
Kameron Kincade <kkincade@mines.edu>
2013-09-27 15:32:05 -0600

    Initializing repo

---
+++ gen/edu/mines/gomezkincadevoicememorecorder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.mines.gomezkincadevoicememorecorder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
